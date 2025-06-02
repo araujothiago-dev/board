85a2e9cea24daeadeb4bb05670b4ae7564322c0e
thiago.machado <thiago.machado@seati.ma.gov.br>
2025-06-02 17:56:46 -0300

    implementa exceptions customisadas

---
+++ src/main/java/br/com/dio/exception/CardFinishException.java
@@ -0,0 +1,7 @@
+package br.com.dio.exception;
+
+public class CardFinishException extends RuntimeException {
+    public CardFinishException(final String message) {
+        super(message);
+    }
+}
