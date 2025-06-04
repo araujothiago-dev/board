1c44eca0ee85ef346bbc54dec965516050efe22d
thiago.machado <thiago.machado@seati.ma.gov.br>
2025-06-04 18:02:09 -0300

    corrige insert de boardColumn para inserir board_id

--- src/main/java/br/com/dio/persistence/dao/BoardColumnDAO.java
+++ src/main/java/br/com/dio/persistence/dao/BoardColumnDAO.java
@@ -19,13 +19,13 @@ public class BoardColumnDAO {
     private final Connection connection;
 
     public BoardColumnEntity insert(final BoardColumnEntity entity) throws SQLException {
-        var sql = "INSERT INTO BOARDS_COLUMNS (name, `order`, kind, board_id) values (?, ?, ?, ?);";
+        var sql = "INSERT INTO BOARDS_COLUMNS (name, `order`, kind, board_id) VALUES (?, ?, ?, ?);";
         try (var statement = connection.prepareStatement(sql)) {
-            var i =1;
+            var i = 1;
             statement.setString(i++, entity.getName());
             statement.setInt(i++, entity.getOrder());
             statement.setString(i++, entity.getKind().name());
-            statement.setLong(i++, entity.getId());
+            statement.setLong(i, entity.getBoard().getId());
             statement.executeUpdate();
             if (statement instanceof StatementImpl impl) {
                 entity.setId(impl.getLastInsertID());
